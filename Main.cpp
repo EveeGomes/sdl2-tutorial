9c6ae9b1cedb04e49d6cb216b7137c383f5dbd27
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-06-15 22:15:00 -0300

    Create a window. Leave it open for 3 sec

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,10 @@
 
 int main(int argc, char* argv[])
 {
-   // To check whether SDL is working, use if statement and call a function:
+   // Create window data type
+   SDL_Window* window = nullptr;
+
+   // Initialize window
    if (SDL_INIT_VIDEO < 0)
    {
       std::cout << "SDL could not be initialized: "
@@ -16,6 +19,20 @@ int main(int argc, char* argv[])
    {
       std::cout << "SDL is ready to go.\n";
    }
+
+   // Create the window
+   window = SDL_CreateWindow("C++ SDL2 Window", 
+                              20,
+                              20,
+                              640,
+                              480,
+                              SDL_WINDOW_SHOWN);
+
+   SDL_Delay(3000);
+
+   SDL_DestroyWindow(window);
+
+   SDL_Quit();
 
    return 0;
 }
