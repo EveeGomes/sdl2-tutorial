63b91b288e80e0ebb05a5d72437adac8f2c2bcad
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-06-15 22:36:36 -0300

    Add comments and application loop that has an event loop and how to handle events.

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,18 @@
 
 int main(int argc, char* argv[])
 {
-   // Create window data type
+   /** 
+   * Create window data type
+   * This pointer will point to the window that is allocated
+   *  from SDL_CreateWindow.
+   */
    SDL_Window* window = nullptr;
 
-   // Initialize window
+   /** 
+   * Initialize the video subsystem.
+   * If it returns less than 1, then an error code will
+   *  be received.
+   */
    if (SDL_INIT_VIDEO < 0)
    {
       std::cout << "SDL could not be initialized: "
@@ -20,7 +28,9 @@ int main(int argc, char* argv[])
       std::cout << "SDL is ready to go.\n";
    }
 
-   // Create the window
+   /** 
+   * Request a window to be created for our platform.
+   */
    window = SDL_CreateWindow("C++ SDL2 Window", 
                               20,
                               20,
@@ -28,10 +38,34 @@ int main(int argc, char* argv[])
                               480,
                               SDL_WINDOW_SHOWN);
 
-   SDL_Delay(3000);
+   // Infinite loop for our application
+   bool gameIsRunning = true;
+   while (gameIsRunning)
+   {
+      SDL_Event event;
+      // Start our event loop
+      while (SDL_PollEvent(&event))
+      {
+         // Handle each specific event
+         if (event.type == SDL_QUIT)
+         {
+            gameIsRunning = false;
+         }
+      }
+   }
 
+   /** 
+   * We destroy our window. We are passing in the pointer that
+   *  points to the memory allocated by the 'SDL_CreateWindow'
+   *  function. Remember, this is a 'C-Style' API, we don't have
+   *  destructors!
+   */
    SDL_DestroyWindow(window);
 
+   /** 
+   * We safely uninitialize SDL2, that is, we are taking down the
+   *  subsystems here before we exit our propgram.
+   */
    SDL_Quit();
 
    return 0;
