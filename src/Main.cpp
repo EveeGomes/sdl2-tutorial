6a069a348c8b67333718844715f4871403c21258
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-18 15:36:11 -0300

    Ep 55 draw rectangles and check when they intersect

--- src/Main.cpp
+++ src/Main.cpp
@@ -50,20 +50,34 @@ int main(int argc, char* argv[])
    SDL_Renderer* renderer = nullptr;
    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-   // Create two objects to render
-   TexturedRectangle object1(renderer, "./images/digital-illustration-pascal-campion-7.bmp");
-   TexturedRectangle object2(renderer, "./images/digital-illustration-pascal-campion-7.bmp");
+   // Setup a texture
+   // Load up the pixels using a surface (CPU)
+   SDL_Surface* surface = SDL_LoadBMP("./images/digital-illustration-pascal-campion-7.bmp");
+   // Create a texture on the GPU using the surface so that we can later free the surface, ie free the pixels once we're done with them on the CPU.
+   SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+   SDL_FreeSurface(surface);
+
+   // Create a rectangle to draw
+   SDL_Rect rectangle;
+   rectangle.x = 50;
+   rectangle.y = 100;
+   rectangle.w = 160;
+   rectangle.h = 160;
+
+   // Draw a rectangle where the mouse is
+   SDL_Rect rectMouse;
+   rectMouse.x = 50;
+   rectMouse.y = 100;
+   rectMouse.w = 160;
+   rectMouse.h = 160;
+
+   // A rectangle to show where the previous rectangles intersect with each other, indicating there was a collision
+   SDL_Rect rectIntersect;
 
    // Infinite loop for our application
    bool gameIsRunning = true;
    while (gameIsRunning)
    {
-      // Capture mouse positions
-      int mouseX;
-      int mouseY;
-      Uint32 buttons;
-      buttons = SDL_GetMouseState(&mouseX, &mouseY);
-
       //(1) Handle Input
       SDL_Event event;
       // Start our event loop
@@ -74,20 +88,13 @@ int main(int argc, char* argv[])
          {
             gameIsRunning = false;
          }
-         // Detect collision from our two shapes if mouse button is pressed
-         if (event.button.button == SDL_BUTTON_LEFT)
-         {
-            // Since object2 is the one we're moving around, we'll work with it!
-            if (object2.IsColliding(object1))
-            {
-               std::cout << "Is colliding\n";
-            }
-            else
-            {
-               std::cout << "Not colliding\n";
-            }
-         }
       }
+      // Populate mouse coordinates
+      int mouseX;
+      int mouseY;
+      Uint32 buttons = SDL_GetMouseState(&mouseX, &mouseY);
+      rectMouse.x = mouseX;
+      rectMouse.y = mouseY;
 
       // (2) Handle Updates
 
@@ -99,14 +106,29 @@ int main(int argc, char* argv[])
 
       // Do our drawing
       SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+      // Draw rectangle
+      SDL_RenderDrawRect(renderer, &rectangle);
 
-      // Set draw positions and sizes
-      object1.Draw(50, 50, 100, 100);
-      object2.Draw(mouseX, mouseY, 100, 100);
+      //// Draw our rotated texture
+      //SDL_Point center;
+      //center.x = rectangle.w / 2;
+      //center.y = rectangle.h / 2;
+      //static int angle = 0;
+      //angle++;
+      //
+      //SDL_RenderCopyEx(renderer, texture, nullptr, &rectangle, angle, &center, SDL_FLIP_NONE);
 
-      // Render our objects
-      object1.Render(renderer);
-      object2.Render(renderer);
+      // Draw our rectangle at our mouse
+      // (color it differently if we collide)
+      if (SDL_IntersectRect(&rectangle, &rectMouse, &rectIntersect))
+      {
+         SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+      }
+      else
+      {
+         SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+      }
+      SDL_RenderDrawRect(renderer, &rectMouse);
 
       // Finally show what we've drawn
       SDL_RenderPresent(renderer);
