f7b0da08810ea131e951bc1c33c086f752231986
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-18 17:17:46 -0300

    Implement some functions in SDLApp. Use std::function to create function pointers of callback functions

--- include/SDLApp.h
+++ include/SDLApp.h
@@ -11,13 +11,28 @@ private:
    SDL_Window* m_window = nullptr;
    // Pointer to our SDL Renderer
    SDL_Renderer* m_renderer = nullptr;
+   // Infinite loop for our application
+   bool m_gameIsRunning = true;
+
+   // Store our callback functions (std::function let us work with function pointers)
+   std::function<void(void)> m_RenderCallback;
+   std::function<void(void)> m_EventCallback;
 
 public:
    // Constructor
-   SDLApp(char* title, int x, int y, int w, int h);
+   SDLApp(const char* title, int x, int y, int w, int h);
 
    // Destructor
    ~SDLApp();
+
+   // Handle events
+   void EventCallback();
+
+   // Handle render
+   void RenderCallback();
+
+   // Loop our application
+   void RunLoop();
 };
 
 #endif // !SDL_APP_H
--- src/Main.cpp
+++ src/Main.cpp
@@ -10,110 +10,17 @@
 // My classes
 #include "TexturedRectangle.h" // API
 #include "AnimatedSprite.h"
+#include "SDLApp.h"
 
 int main(int argc, char* argv[])
 {
-   
+   // Create an instance of SDLApp so everything that happens in our program, happens through an instance of SDLApp.
+   SDLApp app{ "New SDL2 Abstraction", 20, 20, 640, 480 };
+   app.RunLoop();
 
-   // Setup a texture
-   // Load up the pixels using a surface (CPU)
-   SDL_Surface* surface = SDL_LoadBMP("./images/digital-illustration-pascal-campion-7.bmp");
-   // Create a texture on the GPU using the surface so that we can later free the surface, ie free the pixels once we're done with them on the CPU.
-   SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-   SDL_FreeSurface(surface);
-
-   // Create a rectangle to draw
-   SDL_Rect rectangle;
-   rectangle.x = 50;
-   rectangle.y = 100;
-   rectangle.w = 160;
-   rectangle.h = 160;
-
-   // Draw a rectangle where the mouse is
-   SDL_Rect rectMouse;
-   rectMouse.x = 50;
-   rectMouse.y = 100;
-   rectMouse.w = 160;
-   rectMouse.h = 160;
-
-   // A rectangle to show where the previous rectangles intersect with each other, indicating there was a collision
-   SDL_Rect rectIntersect;
-
-   // Infinite loop for our application
-   bool gameIsRunning = true;
-   while (gameIsRunning)
-   {
-      //(1) Handle Input
-      SDL_Event event;
-      // Start our event loop
-      while (SDL_PollEvent(&event))
-      {
-         // Handle each specific event
-         if (event.type == SDL_QUIT)
-         {
-            gameIsRunning = false;
-         }
-      }
-      // Populate mouse coordinates
-      int mouseX;
-      int mouseY;
-      Uint32 buttons = SDL_GetMouseState(&mouseX, &mouseY);
-      rectMouse.x = mouseX;
-      rectMouse.y = mouseY;
-
-      // (2) Handle Updates
-
-      // (3) Clear and Draw the Screen
-      // Set a color before clearing: set the background color
-      SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-      // Gives us a clear "canvas"
-      SDL_RenderClear(renderer);
-
-      // Do our drawing
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-      // Draw rectangle
-      SDL_RenderDrawRect(renderer, &rectangle);
-
-      // Draw our rotated texture
-      SDL_Point center;
-      // This is the local rotation within the 'rectangle'. Rotation from the top-left corner
-      //center.x = 0;
-      //center.y = 0;
-      
-      // Rotation from the center of the rectangle
-      center.x = rectangle.w / 2;
-      center.y = rectangle.h / 2;
-      static int angle = 0;
-      angle++;
-      
-      SDL_RenderCopyEx(renderer, texture, nullptr, &rectangle, angle, &center, SDL_FLIP_NONE); // SDL2
-      // in SDL3 use SDL_RenderTextureRotated() passing the same parameters! @https://wiki.libsdl.org/SDL3/SDL_RenderTextureRotated
-
-      // Draw our rectangle at our mouse
-      // (color it differently if we collide)
-      if (SDL_IntersectRect(&rectangle, &rectMouse, &rectIntersect))
-      {
-         SDL_SetRenderDrawColor(renderer, 255, 0, 255, SDL_ALPHA_OPAQUE);
-         // Change rectangle color when intersect by the rectMouse
-         SDL_RenderDrawRect(renderer, &rectIntersect);
-         SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-      }
-      else
-      {
-         SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-      }
-      SDL_RenderDrawRect(renderer, &rectMouse);
-
-      // Finally show what we've drawn
-      SDL_RenderPresent(renderer);
-
-      /** 
-      * This is how many milliseconds we want to slowdown the program
-      */
-      SDL_Delay(100);
-   }
-
-
+   // Create two objects to render
+   TexturedRectangle object1(renderer, "./images/digital-illustration-pascal-campion-7.bmp");
+   TexturedRectangle object2(renderer, "./images/digital-illustration-pascal-campion-7.bmp");
 
    /** 
    * We safely uninitialize SDL2, that is, we are taking down the
