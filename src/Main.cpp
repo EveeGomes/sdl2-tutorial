c16dd744941c965c0184a2b55cb6c73ccbc108e5
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-20 16:37:34 -0300

    Make objects global pointers and instanciate them in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,6 +16,12 @@
 // One possibility of creating as a global (that should be avoided, but for learning/teaching purpose we'll use for now)
 SDLApp* app = nullptr;
 
+// Make them global for now...
+// Eventually, we will want some sort of factory (or some other way) to manage object creation in our app.
+// Create two objects to render
+TexturedRectangle* object1;
+TexturedRectangle* object2;
+
 void HandleEvents()
 {
    //(1) Handle Input
@@ -29,48 +35,52 @@ void HandleEvents()
       {
          app->StopAppLoop();
       }
-      //// Detect collision from our two shapes if mouse button is pressed
-      //if (event.button.button == SDL_BUTTON_LEFT)
-      //{
-      //   // Since object2 is the one we're moving around, we'll work with it!
-      //   if (object2.IsColliding(object1))
-      //   {
-      //      std::cout << "Is colliding\n";
-      //   }
-      //   else
-      //   {
-      //      std::cout << "Not colliding\n";
-      //   }
-      //}
+      // Detect collision from our two shapes if mouse button is pressed
+      if (event.button.button == SDL_BUTTON_LEFT)
+      {
+         // Since object2 is the one we're moving around, we'll work with it!
+         if (object2->IsColliding(*object1))
+         {
+            std::cout << "Is colliding\n";
+         }
+         else
+         {
+            std::cout << "Not colliding\n";
+         }
+      }
    }
 }
 
 void HandleRendering()
 {
-   // Create two objects to render
-   TexturedRectangle object1(app->GetRenderer(), "./images/digital-illustration-pascal-campion-7.bmp");
-   TexturedRectangle object2(app->GetRenderer(), "./images/digital-illustration-pascal-campion-7.bmp");
-
    // Set draw positions and sizes
-   object1.Draw(50, 50, 100, 100);
-   object2.Draw(app->GetMouseX(), app->GetMouseY(), 100, 100);
+   object1->Draw(50, 50, 100, 100);
+   object2->Draw(app->GetMouseX(), app->GetMouseY(), 100, 100);
 
    // Render our objects
-   object1.Render(app->GetRenderer());
-   object2.Render(app->GetRenderer());
+   object1->Render(app->GetRenderer());
+   object2->Render(app->GetRenderer());
 }
 
 int main(int argc, char* argv[])
 {
    // Setup the SDLApp
    app = new SDLApp{ "New SDL2 Abstraction", 20, 20, 640, 480 };
+
+   // Create any objects in our scene
+   object1 = new TexturedRectangle{ app->GetRenderer(), "./images/digital-illustration-pascal-campion-7.bmp" };
+   object2 = new TexturedRectangle{ app->GetRenderer(), "./images/digital-illustration-pascal-campion-7.bmp" };
+
    // Set the callback functions
    app->SetEventCallback(HandleEvents);
    app->SetRenderCallback(HandleRendering);
    // Run our application until terminated
    app->RunLoop();
 
+   // Clean up our application
    delete app;
+   delete object1;
+   delete object2;
 
    return 0;
 }
